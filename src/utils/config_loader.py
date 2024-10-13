5df6275a78891b02d90491438556c0287b30d905
Antiloope <rodrigopizarro1234@gmail.com>
2024-10-13 17:47:25 -0300

    Changes DB connection loader to read from env

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,6 +1,7 @@
 from application_context import ApplicationContext
+from os import environ
 
 def load_config_on_application_context(application_context: ApplicationContext):
     application_context.config = {
-        "DATABASE_URL": "postgresql://postgres:mysecretpassword@db:5432/mydatabase"
+        "DATABASE_URL": environ.get("DATABASE_URL")
     }
